bfd1a18ccc17ab084aaf51c55ca7749db5dc4fc8
kofimanful-drsmile <92644086+kofimanful-drsmile@users.noreply.github.com>
2022-01-05 23:11:57 +0100

    search heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -42,6 +42,19 @@ export class HeroService {
       );
   }
 
+  searchHeroes(term: string): Observable<Hero[]>{
+    if (!term.trim()){
+      //if no search term, return empty hero array
+      return of([]);
+    }
+    return this.httpProperty.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x=>x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes',[]))
+    );
+  }
+
 
   private heroesUrl ='api/heroes';
 
